b30dcf8d4386844f500e4c5e7a6480885fb3daaa
Daniel Spinosa <spinosa@gmail.com>
2018-06-29 16:01:45 -0400

    add webcredentials to apple association file

--- app/controllers/well_known_controller.rb
+++ app/controllers/well_known_controller.rb
@@ -10,6 +10,9 @@ class WellKnownController < ApplicationController
                           "paths": ["*"]
                         }
                       ]
+                    },
+                    "webcredentials": {
+                      "apps": [ "8C9UTG7ML3.com.cromulentconsulting.challenge" ]
                     }
                   }
   end
